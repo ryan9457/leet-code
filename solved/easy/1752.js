595b1bdbf89852e623cd31e404acabfaff3442fd
Ryan <>
2025-07-02 20:08:00 +0800

    other: move solved 1752 script to easy path

--- solved/easy/1752.js
+++ solved/easy/1752.js
@@ -2,15 +2,15 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-function(nums) {
+function check(nums) {
     let newArray = [...nums]
-    const sortArray = nums.sort((a,b)=> a-b)
-    for(let i = 0 ; i< nums.length ; i++) {
+    const sortArray = nums.sort((a, b) => a - b)
+    for (let i = 0; i < nums.length; i++) {
         const rest = newArray.slice(1)
         newArray = [...rest, newArray[0]]
-        if(JSON.stringify(sortArray) === JSON.stringify(newArray)) {
+        if (JSON.stringify(sortArray) === JSON.stringify(newArray)) {
             return true
         }
     }
     return false
-};
+}
